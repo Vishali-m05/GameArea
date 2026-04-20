cf924d1fb17c8623d5f85dd218c99cc60fb29e18
Vishali <22b01a05a4@svecw.edu.in>
2026-04-20 23:21:11 +0530

    added root route

--- server/server.js
+++ server/server.js
@@ -22,7 +22,9 @@ app.use(express.json());
 app.use("/api/chat", chatRoutes);
 
 const PORT = 5000;
-
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
