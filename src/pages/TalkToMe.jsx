2611bc095358cbeea2650f4bb0cccc71b5a4b88d
Vishali <22b01a05a4@svecw.edu.in>
2026-04-21 11:44:28 +0530

    fixed API endpoint

--- src/pages/TalkToMe.jsx
+++ src/pages/TalkToMe.jsx
@@ -25,7 +25,7 @@ const TalkToMe = () => {
     setIsTyping(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/chat", {
+      const response = await fetch("https://gamearea-1.onrender.com/api/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
